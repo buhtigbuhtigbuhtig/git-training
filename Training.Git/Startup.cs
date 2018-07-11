1d00bed847e8ba6cc94572a7ae0595934ae78ced
buhtigbuhtigbuhtig <yonghui.li@seloger.com>
2018-07-11 11:41:03 +0200

    Mon premier commit

--- Training.Git/Startup.cs
+++ Training.Git/Startup.cs
@@ -30,7 +30,7 @@ namespace Training.Git
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+			services.AddMemoryCache();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
